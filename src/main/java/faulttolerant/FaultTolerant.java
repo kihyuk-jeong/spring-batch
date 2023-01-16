5233020c7ede6e9dbb183d3aca0b051985de312f
kihyuk.jeong <kihyuk.jeong@musinsa.com>
2023-01-16 21:34:03 +0900

    faultTolerant

---
+++ src/main/java/faulttolerant/FaultTolerant.java
@@ -0,0 +1,77 @@
+package faulttolerant;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.adapter.ItemWriterAdapter;
+import org.springframework.batch.item.support.ClassifierCompositeItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
+import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.repeat.RepeatCallback;
+import org.springframework.batch.repeat.RepeatContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
+import org.springframework.batch.repeat.support.RepeatTemplate;
+import org.springframework.classify.PatternMatchingClassifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Configuration
+public class FaultTolerantConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(5)
+                .reader(new ItemReader<String>() {
+                    int i = 0;
+                    @Override
+                    public String read() {
+                        i++;
+                        if(i == 1) {
+                            throw new IllegalArgumentException("skip");
+                        }
+                        return i > 3 ? null : "item" + i;
+                    }
+                })
+                .processor((ItemProcessor<String, String>) item -> {
+                    throw new IllegalStateException("retry");
+//                    return item;
+                })
+                .writer(System.out::println)
+                .faultTolerant()
+                .skip(IllegalArgumentException.class)
+                .skipLimit(1)
+                .retry(IllegalStateException.class)
+                .retryLimit(2)
+                .build();
+    }
+
+    @Bean
+    public SimpleLimitExceptionHandler simpleLimitExceptionHandler(){
+        return new SimpleLimitExceptionHandler(3);
+    }
+}
